09d8342d2b9857376eefa768fda11c0834259c6d
Abou Diaby <ab.diaby@gmail.com>
2019-12-13 16:55:52 -0600

    modified the path

--- Tutorial_web/Tutorial/obj/Release/Package/PackageTmp/sciptfile/Untitled3.ps1
+++ Tutorial_web/Tutorial/obj/Release/Package/PackageTmp/sciptfile/Untitled3.ps1
@@ -2,7 +2,7 @@
 
 $msbuild = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 $options = "/p:PublishProfile='CustomProfile.pubxml' /p:VisualStudioVersion=14.0  /p:AspnetMergePath='C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools' /p:WebPublishMethod=FileSystem /p:DeleteExistingFiles=True  /p:DeployOnBuild=true" 
-$proj = "'C:\TeamCity\buildAgent\work\3889bb1e2d3cb27a\Tutorial_web\Tutorial\Tutorial.csproj'"
+$proj = "'C:\Users\MNdev\source\repos\Tutorial\Tutorial\Tutorial.csproj'"
 
 # Execute
 $build = $msbuild + " $proj " + $options
