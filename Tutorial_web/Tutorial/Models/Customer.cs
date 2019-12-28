191fc71e14fe80d7d4f7f8484837c5c14cd77cc4
Abou Diaby <ab.diaby@gmail.com>
2019-12-28 10:24:06 -0600

    expand the entry customer  form

--- Tutorial_web/Tutorial/Models/Customer.cs
+++ Tutorial_web/Tutorial/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace Tutorial.Models
     {
         public string CustomerId { get; set; }
         public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
     }
 }
