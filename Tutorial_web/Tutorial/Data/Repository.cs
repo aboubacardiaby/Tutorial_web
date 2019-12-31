52a9b9a8602c0451d292d76d5e0d085cdeafddad
Abou Diaby <ab.diaby@gmail.com>
2019-12-30 21:57:42 -0600

    add database creation

--- Tutorial_web/Tutorial/Data/Repository.cs
+++ Tutorial_web/Tutorial/Data/Repository.cs
@@ -10,6 +10,10 @@ namespace Tutorial.Data
         workerEntities1 _entities = new workerEntities1();
         public void AddNewCustomer(Customer customer)
         {
+           if( !_entities.Database.Exists())
+            {
+                _entities.Database.CreateIfNotExists();
+            }
             _entities.Customers.Add(customer);
             _entities.SaveChanges();
         }
